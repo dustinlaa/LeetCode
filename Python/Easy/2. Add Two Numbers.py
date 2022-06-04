fea9819701efac2ce760d1acdb419308ccb15ec9
Dustin La <drl3@njit.edu>
2022-06-04 11:48:44 -0400

    made small comment fix

--- Python/Easy/2. Add Two Numbers.py
+++ Python/Easy/2. Add Two Numbers.py
@@ -73,6 +73,7 @@ class Solution(object):
 ##############################################################   
         return rtnList
 
+############## LOCAL TESTING ONLY ############################   
     def __init__(self, l1, l2):
         self.l1 = l1
         self.l2 = l2
